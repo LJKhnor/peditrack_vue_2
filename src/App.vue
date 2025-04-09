819975a1d77919cafee60bd7349c3c88587d150c
LJKhnor <135867702+LJKhnor@users.noreply.github.com>
2025-04-09 22:07:54 +0200

    add map and test marker

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/appointment">Rendez-vous</RouterLink>
       <RouterLink to="/patient">Patient</RouterLink>
+      <RouterLink to="/map">Map</RouterLink>
       <!-- <RouterLink to="/data">Données</RouterLink> -->
-      <!-- <RouterLink to="/map">Map</RouterLink> -->
     </nav>
   </header>
   <main>
--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,63 +1,8 @@
-<script>
-import { onMounted } from 'vue'
-import apiClient from '../axios'
-import 'leaflet/dist/leaflet.css'
-import { LMap, LTileLayer, LMarker, LCircle } from '@vue-leaflet/vue-leaflet'
-
-export default {
-  components: {
-    LMap,
-    LTileLayer,
-    LMarker,
-    LCircle
-  },
-  setup() {
-    let zoom = 13
-    let iconWidth = 25
-    let iconHeight = 40
-    const urlGetAllPatient = '/patients'
-    const options = {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': 'http://localhost:5173' // Allow requests from your Vue.js frontend
-        // Add any other headers if needed
-      }
-    }
-    function iconUrl() {
-      return `https://placekitten.com/${this.iconWidth}/${this.iconHeight}`
-    }
-    function iconSize() {
-      return [this.iconWidth, this.iconHeight]
-    }
-    onMounted(() => {
-      getAllPatient()
-    })
-
-    async function getAllPatient() {
-      const response = await apiClient.get(urlGetAllPatient, options)
-
-      console.log(response.data)
-      response.data.forEach((element) => {
-        rowData.value.push({
-          lastName: element.name,
-          firstname: element.firstname,
-          phoneNum: element.phoneNum,
-          birthdate: element.birthdate,
-          address: element.address
-        })
-      })
-      console.log('rowData', rowData)
-    }
-    return { iconUrl, iconSize, zoom, iconWidth, iconHeight }
-  }
-}
-</script>
 <template>
   <div class="map">
     <h1>Carte</h1>
-    <div style="height: 600px; width: 800px">
-      <l-map ref="map" v-model:zoom="zoom" :center="[50.4526514, 4.8884249]">
+    <div id="mapContainer" style="height: 600px; width: 800px">
+      <!-- <l-map ref="map" v-model:zoom="zoom" :center="[50.4526514, 4.8884249]">
         <l-tile-layer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           layer-type="base"
@@ -85,10 +30,91 @@ export default {
           :fill="false"
           opacity="0.5"
         ></l-circle>
-      </l-map>
+      </l-map> -->
     </div>
   </div>
 </template>
+
+<script>
+import { onMounted, onUnmounted } from 'vue'
+import apiClient from '../axios'
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+import { LMap, LTileLayer, LMarker, LCircle } from '@vue-leaflet/vue-leaflet'
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LCircle
+  },
+  setup() {
+    let zoom = 13
+    let iconWidth = 25
+    let iconHeight = 40
+    let listPoints = [
+      [50.4526514, 4.8884249],
+      [50.45, 4.88]
+    ]
+    const urlGetAllPatientPosition = '/data/map'
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:5173' // Allow requests from your Vue.js frontend
+        // Add any other headers if needed
+      }
+    }
+    function iconUrl() {
+      return `https://placekitten.com/${this.iconWidth}/${this.iconHeight}`
+    }
+    function iconSize() {
+      return [this.iconWidth, this.iconHeight]
+    }
+    onMounted(() => {
+      // on créé le layer pour acceuillir la map et on lui passe les infos de centrage et de zoom
+      let map = L.map('mapContainer').setView([50.4526514, 4.8884249], zoom)
+      // on lui passe les tuilles de la map
+      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map)
+      let iconStartingPoint = L.icon({
+        iconUrl: 'logo_pedicure_val.png'
+      })
+      let startingPoint = L.marker([50.4526514, 4.8884249], { icon: iconStartingPoint }).addTo(map)
+
+      getAllPatientPositions(map)
+    })
+
+    async function getAllPatientPositions(map) {
+      createCatchmentArea(map)
+      // const response = await apiClient.get(urlGetAllPatient, options)
+      // for (const point in listPoints) {
+      //   L.marker(listPoints[point]).addTo(map)
+      // }
+    }
+    function createCatchmentArea(map) {
+      let circle1 = L.circle([50.4526514, 4.8884249], {
+        color: 'green',
+        fillOpacity: 0.0,
+        radius: 5000
+      }).addTo(map)
+      let circle2 = L.circle([50.4526514, 4.8884249], {
+        color: 'yellow',
+        fillOpacity: 0.0,
+        radius: 10000
+      }).addTo(map)
+      let circle3 = L.circle([50.4526514, 4.8884249], {
+        color: 'red',
+        fillOpacity: 0.0,
+        radius: 15000
+      }).addTo(map)
+    }
+    return { iconUrl, iconSize, zoom, iconWidth, iconHeight }
+  }
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
