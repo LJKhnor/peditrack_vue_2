deb53e2b361447b27e8601369924f7e62249ac1d
LJKhnor <135867702+LJKhnor@users.noreply.github.com>
2026-03-09 14:59:56 +0100

    fix vue logique

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,3 +3,8 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+declare module '../axios' {
+  import { AxiosInstance } from 'axios'
+  const apiClient: AxiosInstance
+  export default apiClient
+}
